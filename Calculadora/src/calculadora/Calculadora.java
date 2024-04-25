9657cded94c15391f6194f31217cc10ffa5330e0
BeccaDev <130665887+RebecadeMouraMendes@users.noreply.github.com>
2024-04-25 11:49:50 -0300

    Update Calculadora.java

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -5,13 +5,13 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Calculadora extends JFrame {
-    JLabel rotulo1, rotulo2, exibir, exibir2;
+    JLabel rotulo1, rotulo2, exibir, exibir2, titulo;
     JTextField texto1,texto2;
-    JButton somar, subtrair, multiplicar, dividir;
+    JButton somar, subtrair, multiplicar, dividir, limpar;
     ImageIcon icone1, icone2, icone3, icone4;
 
     public Calculadora(){
-    super("Operações com dois números");
+    super("Calculadora");
     Container tela = getContentPane();
     setLayout(null);
     setResizable(false);
@@ -20,6 +20,7 @@ public class Calculadora extends JFrame {
     icone2 = new ImageIcon("sub.png");
     icone3 = new ImageIcon("mul.png");
     icone4 = new ImageIcon("div.png");
+    titulo = new JLabel("Operações com dois números ");
     rotulo1 = new JLabel("Primeiro número: ");
     rotulo2 = new JLabel("Segundo número: ");
     texto1 = new JTextField(5);
@@ -31,23 +32,25 @@ public class Calculadora extends JFrame {
     multiplicar = new JButton(icone3);
     dividir = new JButton(icone4);
     
+    titulo.setFont(new Font("Arial",Font.BOLD,18));
     //Localização na tela
-    rotulo1.setBounds(40,20,200,20);
-    rotulo2.setBounds(40,60,200,20);
-    texto1.setBounds(180,20,200,20);
-    texto2.setBounds(180,60,200,20);
-    exibir.setBounds(40,160,300,20);
-    exibir2.setBounds(40,180,300,20);
-    somar.setBounds(40,110,80,40);
-    subtrair.setBounds(150,110,80,40);
-    multiplicar.setBounds(260,110,80,40);
-    dividir.setBounds(370,110,80,40);
-    
+    titulo.setBounds(110,10,300,20);
+    rotulo1.setBounds(40,40,200,20);
+    rotulo2.setBounds(40,70,200,20);
+    texto1.setBounds(150,40,280,20);
+    texto2.setBounds(150,70,280,20);
+    exibir.setBounds(40,170,350,20);
+    exibir2.setBounds(40,190,350,20);
+    somar.setBounds(120,110,40,40);
+    subtrair.setBounds(190,110,40,40);
+    multiplicar.setBounds(260,110,40,40);
+    dividir.setBounds(330,110,40,40);
+    //Cores 
     somar.setBackground(new Color(127, 227, 102));
     subtrair.setBackground(new Color(242, 235, 107));
     multiplicar.setBackground(new Color(237, 95, 85));
     dividir.setBackground(new Color(74, 166, 232));
-    //Ação
+    //Ação somar
     somar.addActionListener(
             new ActionListener(){
                 public void actionPerformed(ActionEvent e){
@@ -57,12 +60,12 @@ public class Calculadora extends JFrame {
                     numero2 = Integer.parseInt(texto2.getText());
                     soma = numero1 + numero2;
                     exibir.setVisible(true);
-                    exibir.setText("A soma é: "+soma);
+                    exibir.setText("A soma de "+numero1 +" + "+numero2+ " é: "+soma);
                     exibir2.setText("");
                 }
             }
     );
-    
+    //Ação subtrair
     subtrair.addActionListener(
             new ActionListener(){
                 public void actionPerformed(ActionEvent e){
@@ -80,7 +83,7 @@ public class Calculadora extends JFrame {
                 }
             }
     );
-    
+    //Ação multiplicar
     multiplicar.addActionListener(
             new ActionListener(){
                 public void actionPerformed(ActionEvent e){
@@ -90,12 +93,12 @@ public class Calculadora extends JFrame {
                     numero2 = Integer.parseInt(texto2.getText());
                     mul = numero1 * numero2;
                     exibir.setVisible(true);    
-                    exibir.setText("A multiplicação é: "+mul);
+                    exibir.setText("A multiplicação de "+numero1 +" * "+numero2+ " é: "+mul);
                     exibir2.setText("");
                 }
             }
     );
-    
+    //Ação dividir
     dividir.addActionListener(
             new ActionListener(){
                 public void actionPerformed(ActionEvent e){
@@ -117,6 +120,7 @@ public class Calculadora extends JFrame {
     );
     exibir.setVisible(false);
     //Adicionando na tela
+    tela.add(titulo);
     tela.add(rotulo1);
     tela.add(rotulo2);
     tela.add(texto1);
@@ -127,6 +131,7 @@ public class Calculadora extends JFrame {
     tela.add(subtrair);
     tela.add(multiplicar);
     tela.add(dividir);
+    //Tela background
     tela.setBackground(new Color(224, 222, 223));
     //Terminando de setar
     setSize(490,300);
@@ -139,4 +144,7 @@ public class Calculadora extends JFrame {
        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
+    //Calculadora com apagar/ocultar/exibir/desabilitar/habilitar
+    
 }
+
