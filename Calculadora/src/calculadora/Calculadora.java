aaaca24ff0ba1c4599494c128b5f464e97b24328
BeccaDev <130665887+RebecadeMouraMendes@users.noreply.github.com>
2024-04-29 18:19:27 -0300

    Update Calculadora.java

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -7,8 +7,8 @@ import java.awt.event.*;
 public class Calculadora extends JFrame {
     JLabel rotulo1, rotulo2, exibir, exibir2, titulo;
     JTextField texto1,texto2;
-    JButton somar, subtrair, multiplicar, dividir, limpar;
-    ImageIcon icone1, icone2, icone3, icone4;
+    JButton somar, subtrair, multiplicar, dividir, apagar, ocultar, exibirbutton, desabilitar, habilitar;
+    ImageIcon icone1, icone2, icone3, icone4, icone5, icone6, icone7, icone8, icone9;
 
     public Calculadora(){
     super("Calculadora");
@@ -20,6 +20,11 @@ public class Calculadora extends JFrame {
     icone2 = new ImageIcon("sub.png");
     icone3 = new ImageIcon("mul.png");
     icone4 = new ImageIcon("div.png");
+    icone5 = new ImageIcon("icone5.png");
+    icone6 = new ImageIcon("icone6.png");
+    icone7 = new ImageIcon("icone7.png");
+    icone8 = new ImageIcon("icone8.png");
+    icone9 = new ImageIcon("icone9.png");
     titulo = new JLabel("Operações com dois números ");
     rotulo1 = new JLabel("Primeiro número: ");
     rotulo2 = new JLabel("Segundo número: ");
@@ -31,20 +36,31 @@ public class Calculadora extends JFrame {
     subtrair = new JButton( icone2);
     multiplicar = new JButton(icone3);
     dividir = new JButton(icone4);
+    apagar = new JButton(icone5);
+    ocultar = new JButton(icone7);
+    exibirbutton = new JButton(icone6);
+    desabilitar = new JButton(icone8);
+    habilitar = new JButton(icone9);
     
     titulo.setFont(new Font("Arial",Font.BOLD,18));
     //Localização na tela
-    titulo.setBounds(110,10,300,20);
-    rotulo1.setBounds(40,40,200,20);
-    rotulo2.setBounds(40,70,200,20);
-    texto1.setBounds(150,40,280,20);
-    texto2.setBounds(150,70,280,20);
+    titulo.setBounds(110,10,400,20);
+    rotulo1.setBounds(40,40,300,20);
+    rotulo2.setBounds(40,70,300,20);
+    texto1.setBounds(175,40,280,20);
+    texto2.setBounds(175,70,280,20);
     exibir.setBounds(40,170,350,20);
     exibir2.setBounds(40,190,350,20);
-    somar.setBounds(120,110,40,40);
-    subtrair.setBounds(190,110,40,40);
-    multiplicar.setBounds(260,110,40,40);
-    dividir.setBounds(330,110,40,40);
+    somar.setBounds(80,110,40,40);
+    subtrair.setBounds(150,110,40,40);
+    multiplicar.setBounds(220,110,40,40);
+    dividir.setBounds(290,110,40,40);
+    
+    apagar.setBounds(380,110,20,20);
+    ocultar.setBounds(380,135,20,20);
+    exibirbutton.setBounds(410,110,20,20);
+    desabilitar.setBounds(410,135,20,20);
+    habilitar.setBounds(440,110,20,20);
     //Cores 
     somar.setBackground(new Color(127, 227, 102));
     subtrair.setBackground(new Color(242, 235, 107));
@@ -118,6 +134,66 @@ public class Calculadora extends JFrame {
                 
             }
     );
+    
+    apagar.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    texto1.setText(null);
+                    texto2.setText(null);
+                    texto1.requestFocus();
+                }
+                
+            }
+    );
+    
+    ocultar.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    texto1.setVisible(false);
+                    texto2.setVisible(false);
+                    rotulo1.setVisible(false);
+                    rotulo2.setVisible(false);
+                }
+                
+            }
+    );
+    
+    exibirbutton.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    texto1.setVisible(true);
+                    texto2.setVisible(true);
+                    rotulo1.setVisible(true);
+                    rotulo2.setVisible(true);
+                }
+                
+            }
+    );
+    
+    desabilitar.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    texto1.setEnabled(false);
+                    texto2.setEnabled(false);
+                    rotulo1.setEnabled(false);
+                    rotulo2.setEnabled(false);
+                }
+                
+            }
+    );
+    
+    habilitar.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    texto1.setEnabled(true);
+                    texto2.setEnabled(true);
+                    rotulo1.setEnabled(true);
+                    rotulo2.setEnabled(true);
+                }
+                
+            }
+    );
+    
     exibir.setVisible(false);
     //Adicionando na tela
     tela.add(titulo);
@@ -131,6 +207,11 @@ public class Calculadora extends JFrame {
     tela.add(subtrair);
     tela.add(multiplicar);
     tela.add(dividir);
+    tela.add(apagar);
+    tela.add(ocultar);
+    tela.add(exibirbutton);
+    tela.add(desabilitar);
+    tela.add(habilitar);
     //Tela background
     tela.setBackground(new Color(224, 222, 223));
     //Terminando de setar
@@ -147,4 +228,3 @@ public class Calculadora extends JFrame {
     //Calculadora com apagar/ocultar/exibir/desabilitar/habilitar
     
 }
-
